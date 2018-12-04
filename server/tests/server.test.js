d4917ccf9dbd5aa313e447f8a67472125a19719a
git <kieronmcc@gmail.com>
2018-12-04 12:37:49 +0100

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,22 +4,31 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+// Fixture data to seed DB
+const todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+
+}];
+
 //This is a text fixture/setup facility in Mocha
 beforeEach((done) => {
   // Expression syntax
   //Todo.remove({}).then (() => done();
   // Statement syntax
   Todo.remove({}).then(() => {
-    console.log('Cleaning database');
-    done();
-  });
+    //console.log('Preparing database for testing');
+    // Returning here allows the callback (i.e. 'then') to be chained
+    return Todo.insertMany(todos);
+  }).then(() => done())  //expression syntax for arrow function
 });
 
-// Test Suite for posted todo
-describe('POST /todos', () => {
+// Test Suite for POST /todos route
+describe('POST /todos Testsuite', () => {
   // Test case for correctly formed todo
   it('should create a new todo', (done) => {
-    console.log('Running Test');
+    //console.log('Running Test');
     var text = 'Test todo text';
     // Using supertest - request
     request(app)
@@ -34,7 +43,8 @@ describe('POST /todos', () => {
         return done(err);
       }
 
-      Todo.find().then((todos) => {
+      // Only find todos that match body set above
+      Todo.find({text}).then((todos) => {
         expect(todos.length).toBe(1);
         expect(todos[0].text).toBe(text);
         done();
@@ -46,7 +56,7 @@ describe('POST /todos', () => {
   it('Should not create a todo with invalid body data', (done) => {
     request(app)
     .post('/todos')
-    .send({})
+    .send({}) // send empty request so fails
     .expect(400)
     .end((err, res) => {
       if (err) {
@@ -54,10 +64,24 @@ describe('POST /todos', () => {
       }
 
       Todo.find().then((todos) => {
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(2);  // because error case so no 3rd added
         done();
       }).catch((e) => done(e));
     });
   });
-// End of testsuite
+// End of testsuite (i.e. describe block)
+});
+
+// Testsuite for GET /todos route
+describe('GET /todos Testsuite', () => {
+  it('should get all todos', (done) => {
+    //SuperTest request
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
+  });
 });
