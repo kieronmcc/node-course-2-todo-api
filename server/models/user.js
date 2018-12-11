6f46141b6ee4cfd4bf68a6019d590701f4747727
git <kieronmcc@gmail.com>
2018-12-11 09:33:07 +0100

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -103,6 +103,26 @@ UserSchema.pre('save', function (next) {
   }
 });
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise ((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (!res) {
+          reject();
+        } else {
+          resolve(user);
+        }
+      });
+    });
+  });
+};
+
 // make User model - email - required, trimmed, type = string minLength 1
 var User = mongoose.model('User', UserSchema);
 
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const {ObjectID} = require('mongodb');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -147,7 +148,7 @@ app.patch('/todos/:id', (req, res) => {
      });
 });
 
-// POST /users - add new user
+// POST /users - add new user (sign up)
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']); //pick function from lodash
   var user = new User(body);
@@ -168,6 +169,21 @@ app.post('/users', (req, res) => {
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
+
+// Route for user login
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']); //pick function from lodash
+
+  User.findByCredentials(body.email, body.password).then ((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+  }).catch ((e) => {
+    res.status(400).send();
+  });
+
+});
+
 
 // server application run listenng on configured port
 app.listen(port, () => {
